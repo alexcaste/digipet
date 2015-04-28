81541d136a3c0c703b22b31e1b546bc44b366bd1
Alex Caste & Frank Johnson <student@epicodus.com>
2015-04-28 11:09:44 -0700

    Writing second spec to creat time passes method is failing

--- spec/digipet_spec.rb
+++ spec/digipet_spec.rb
@@ -5,11 +5,19 @@ require('digipet')
 describe(DigiPet) do
   describe("#initialize") do
     it("creates new pet and sets life levels") do
-    my_pet = DigiPet.new("lil dragon")
-    expect(my_pet.name()).to(eq("lil dragon"))
-    expect(my_pet.food_lvl()).to(eq(10))
-    expect(my_pet.sleep_lvl()).to(eq(10))
-    expect(my_pet.active_lvl()).to(eq(10))
+      my_pet = DigiPet.new("lil dragon")
+      expect(my_pet.name()).to(eq("lil dragon"))
+      expect(my_pet.food_lvl()).to(eq(10))
+      expect(my_pet.sleep_lvl()).to(eq(10))
+      expect(my_pet.active_lvl()).to(eq(10))
+    end
+  end
+
+  describe("#time_passes") do
+    it("descreases the amount of food of the DigiPet has left by 1") do
+      my_pet = DigiPet.new("lil dragon")
+      my_pet.time_passes()
+      expect(my_pet.food_lvl()).to(eq(9))
     end
   end
 end
